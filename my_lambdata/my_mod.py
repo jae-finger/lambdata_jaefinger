280afd40e7bbe6b34fdfa2798adadd217057af0a
Jonathan Finger <jaefinger@gmail.com>
2020-03-12 18:58:56 -0500

    final v2

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -1,17 +1,17 @@
 
 #my_lambdata/my_mod.py
+import pandas
 
-
-def null_test(xlist):
-    isnull = xlist.isnull().sum()
+def null_test(xdf):
+    isnull = xdf.isnull().sum()
     print("Here is a list of columns and the number of missing values:")
     print(isnull)
 
 import pandas
 
-def add_a_col(l, df):
+def add_a_col(l, xdf):
     l = pandas.Series(l)
     print("Converted list to series")
     l = pandas.DataFrame(l)
     print("Converted series to dataframe")
-    df['new_col'] = l
+    xdf['new_col'] = l
