b844b1fa309a29a7fc74833823784c2952fae257
Jonathan Finger <jaefinger@gmail.com>
2020-03-12 13:15:56 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+# setup.py file
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="lambdata-jae-finger",
+    version="1.0",
+    author="JAE Finger",
+    author_email="jaefinger@gmail.com",
+    description="LS DS Unit 3 Assignment 1-1",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    #license="MIT",
+    url="https://github.com/jae-finger/lambdata-jae-finger",
+    #keywords="",
+    packages=find_packages() # ["my_lambdata"]
+)
