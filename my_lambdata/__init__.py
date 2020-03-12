7b0546ecc3a79242b5c3849b48115d561746e4b9
Jonathan Finger <jaefinger@gmail.com>
2020-03-12 12:47:19 -0500

    added init.py file and some code

---
+++ my_lambdata/__init__.py
@@ -0,0 +1,4 @@
+
+#my_lambdata/__init__.py
+
+#Making this because Mike told me it's necessary for importing XD
--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,8 +1,15 @@
 #my_lambdata/my_script.py
 
 import pandas
+from my_lambdata.my_mod import order_of_mag
 
 print("Gotta test this out, yo.")
 
 df = pandas.DataFrame({"a" : [0, 1, 2], "b" : [3, 4, 5]})
 print(df.head())
+
+
+x = 10
+
+print(f"Let's increase x ({x}) by an order of magnitude using a custom function called order_of_mag")
+print(f"Bam-  the function increased x from {x} to {order_of_mag(x)}")
