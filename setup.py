3a7ef3a505c19cd278743638dbc78447338f8f2a
Jonathan Finger <jaefinger@gmail.com>
2020-03-12 21:13:34 -0500

    still working on importing

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambdata_jaefinger",
-    version="2.0",
+    version="2.1",
     author="JAE Finger",
     author_email="jaefinger@gmail.com",
     description="LS DS Unit 3 Assignment 1-1",
     long_description=long_description,
     long_description_content_type="text/markdown", # required if using a md file for long desc
     #license="MIT",
-    url="https://github.com/jae-finger/lambdata-jae-finger",
+    url="https://github.com/jae-finger/lambdata_jaefinger",
     #keywords="",
     packages=find_packages() # ["my_lambdata"]
 )
