7b35da911794e96b04fd3b95d53bb6db0888da9e
Jon Finger <jaefinger@gmail.com>
2020-03-15 21:11:12 -0500

    added more pep8 changes

--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,15 +1,17 @@
-#my_lambdata/my_script.py
-#Here we go ~
+# my_lambdata/my_script.py
 
 import pandas
 from my_lambdata.my_mod import null_test
 from my_lambdata.my_mod import add_a_col
 
-#Utilize the null_test function
-df = pandas.DataFrame({"a" : [0, 1, 2, 3, 4, 5, 6,], "b" : [3, 4, 5, 6, 7, 8, 9], "y" : [100, 10, 100, 5, 0, 50, 10]})
+# Utilize the null_test function
+
+df = pandas.DataFrame({"a": [0, 1, 2, 3, 4, 5, 6,],
+                        "b": [3, 4, 5, 6, 7, 8, 9],
+                        "y": [100, 10, 100, 5, 0, 50, 10]})
 null_test(df)
 
-from my_lambdata.my_mod import add_a_col
+# Check out the add_a_col function
 my_list = [4, 3, 2, 0, 1, 2]
 add_a_col(my_list, df)
 print(df)
