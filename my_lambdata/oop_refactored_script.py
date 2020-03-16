929d489eec00e814e2eba3a2611b853f3c04efc8
Jon Finger <jaefinger@gmail.com>
2020-03-16 18:03:49 -0500

    initial oop script created

--- my_lambdata/oop_refactored_script.py
+++ my_lambdata/oop_refactored_script.py
@@ -1,3 +1,5 @@
+#Refactored script code with OOP approach
+
 import pandas as pd
 import numpy as np 
 
@@ -5,15 +7,22 @@ class NullFinder():
     def __init__(self, num_list):
         self.num_list = num_list
     
-    def findnulls(parameter_list):
+    def findnulls(self):
         is_null = []
-        for x in my_list_with_null:
+        for x in self.num_list:
             if x is np.NaN:
                 is_null.append('Yes')
             else:
                 is_null.append('No')
+        print(is_null)
+
+test_list = [4, 3, 2, 0, np.NaN, 2, 4]
+nullf = NullFinder(test_list)
+
+nullf.findnulls()
 
 
+# Old functional approach notes
 # my_list_with_null = [1, 2, np.NaN]
 # my_list_withouta_null = [3, 4, 5]
 # is_null = []
