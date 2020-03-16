9477f82eff262e193bf679ec96a471da8edf4d13
Jon Finger <jaefinger@gmail.com>
2020-03-15 21:17:48 -0500

    fixed pep8 style recommendations

--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -6,7 +6,9 @@ from my_lambdata.my_mod import add_a_col
 
 # Utilize the null_test function
 
-df = pandas.DataFrame({"a": [0, 1, 2, 3, 4, 5, 6,],
+df = pandas.DataFrame(
+                    {
+                        "a": [0, 1, 2, 3, 4, 5, 6],
                         "b": [3, 4, 5, 6, 7, 8, 9],
                         "y": [100, 10, 100, 5, 0, 50, 10]})
 null_test(df)
